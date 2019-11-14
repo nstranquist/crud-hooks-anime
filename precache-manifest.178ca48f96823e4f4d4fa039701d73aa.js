47fca913ba511d7921ff6c415f6a55f7f2b9c201
Nico Stranquist <nstranquist@gmail.com>
2019-11-14 15:59:01 -0600

    Updates

---
+++ precache-manifest.178ca48f96823e4f4d4fa039701d73aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b54311705f511e90676a380a23e0cfc",
+    "url": "/crud-hooks-anime/index.html"
+  },
+  {
+    "revision": "330a9905276f1250c7f3",
+    "url": "/crud-hooks-anime/static/css/main.3a4057f7.chunk.css"
+  },
+  {
+    "revision": "3e78b86fd067e801364f",
+    "url": "/crud-hooks-anime/static/js/2.d71126f1.chunk.js"
+  },
+  {
+    "revision": "330a9905276f1250c7f3",
+    "url": "/crud-hooks-anime/static/js/main.d27b1070.chunk.js"
+  },
+  {
+    "revision": "c05d5609d86aca7d92df",
+    "url": "/crud-hooks-anime/static/js/runtime-main.7f440afb.js"
+  }
+]);
